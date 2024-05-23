5cd5aa0fe9b283eec9bf7710725bb4939824631e
Belousovaao <belousovaao@mail.ru>
2024-05-23 22:08:51 +0700

    Task 1.4

---
+++ Task 1.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int quantityOfBoxes = 50;
+        while (quantityOfBoxes > 0)
+        {
+            Console.WriteLine("Сколько ящиков готова забрать машина?");
+            int boxes = Convert.ToInt32(Console.ReadLine());
+            if (boxes <= quantityOfBoxes)
+            {
+                quantityOfBoxes -= boxes;
+            }
+            else
+            {
+                Console.WriteLine($"На складе такого количества нет, отгружем {quantityOfBoxes} коробок");
+                quantityOfBoxes = 0;
+            }
+        }
+    }
+}
