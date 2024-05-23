d42f6f3c35bfa005d68e50a07504940660694fdb
Belousovaao <belousovaao@mail.ru>
2024-05-23 21:46:07 +0700

    Task 1.2

---
+++ Task 1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+        Console.WriteLine("Введите число n.");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine(sum);
+    }
+}
