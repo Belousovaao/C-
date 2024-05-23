8ecb7c933f9aa1dbe634b7196568962c6f4b8a9b
Belousovaao <belousovaao@mail.ru>
2024-05-23 21:43:14 +0700

    Задание 1.1

---
+++ Task1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ширину прямоугольника.");
+        int width_p = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите длину прямоугольника.");
+        int length_p = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Периметр прямоугольника равен {width_p * 2 + length_p * 2}");
+        Console.WriteLine($"Площадь прямоугольника равна {width_p * length_p}");
+    }
+}
