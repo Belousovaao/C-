96669dd99099ac844734cfb2567aecf57a1372b5
Belousovaao <belousovaao@mail.ru>
2024-05-28 20:20:04 +0700

    with gitignore

--- Task 1.2/obj/Debug/net8.0/Task 1.2.AssemblyInfo.cs
+++ Task 1.2/obj/Debug/net8.0/Task 1.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 1.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd5aa0fe9b283eec9bf7710725bb4939824631e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbed9eb7ff2a7c2f0c572e8c3ad54cfc0b4c7ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 1.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 1.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
