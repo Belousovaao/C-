e48cabf3afaa6b8ce4d1f0d6c534a40b163e7100
Belousovaao <belousovaao@mail.ru>
2024-06-02 23:37:52 +0700

    task2 ready

--- Task2/Animal.cs
+++ Task2/Animal.cs
@@ -4,37 +4,55 @@
 public abstract class Animal(int age)
 {
     protected int health = 9;
+    private string? name;
+    private bool isNameSet = false;
 
-    public string? Name { get; init; }
-
+    public string? Name
+    {
+        get => name;
+        set
+        {
+            if (!isNameSet)
+            {
+                name = value;
+                isNameSet = true;
+            }
+            else
+            {
+                Console.WriteLine("Имя уже задано!");
+            }
+        }
+    }
     public int Age { get; init; } = age;
 
-    public void Feed (int foodCount)
+    public string Health // реализация через свойство, т.к. речь идет о данных
+    // если бы манипуляции производились с какими-то действиями, выбрала бы метод
     {
-        health += foodCount;
+        get 
+        { 
+            if (health >= 0 && health <= 50)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return "Окрас белый"; 
+                }
+            else if (health >= 51 && health <= 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return "Окрас зелёный";
+                }
+            else
+                {
+                    return "Такое значение невозможно! Видимо, вы перекормили свою кошку";
+                }
+        }
     }
-
-    public void Punish (int punchCount)
-    {
-        health += punchCount;
-    }
-
-   // public string Health
-    //{
-    //    get 
-    //    { 
-    //        if (this.health >= 0 && this.health <= 50)
-    //            return $"Окрас белый";
-    //        else if (this.health >= 51 && this.health <= 100)
-    //            return $"Окрас зелёный";
-    //    }
-    //}
-
     public virtual void Say()
     {
-        if (Name == "")
+        if (Name is null)
             Console.WriteLine($"Привет! Дай мне имя, мне {Age} лет");
         else
             Console.WriteLine($"Привет! Меня зовут {Name}, мне {Age} лет");
     }
+    public virtual void Feed(int foodCount) => health += foodCount;
+    public virtual void Punish(int punchCount) => health -= punchCount;
 }
---
+++ Task2/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Common;
+using System.Security.Cryptography;
+using Task2;
+
+internal class Test
+{
+    private static void Main(string[] args)
+    {
+       Animal[] animals = [new Cat (2), new Dog(3)];
+       foreach (var animal in animals)
+       {
+       if (animal is Cat cat1)
+        {
+            cat1.Say();
+            cat1.Name = "Муся";
+            cat1.Say();
+            //cat1.Age = 5; - ошибка! возраст менять нельзя!
+            cat1.Name = "1"; // Вызывает фразу, что имя изменить нельзя, команда не будет выполнена
+            cat1.Feed(50);
+            cat1.Punish(5);
+            Console.WriteLine(cat1.Health);
+            Console.ResetColor();
+        }
+       
+       else if (animal is Dog dog1)
+        {
+            dog1.Say();
+            dog1.Name = "Шарик";
+            dog1.Say();
+            //dog1.Age = 5; - ошибка! возраст менять нельзя!
+            dog1.Name = "1"; // Вызывает фразу, что имя изменить нельзя, команда не будет выполнена
+            dog1.Feed(3);
+            dog1.Punish(5);
+            Console.WriteLine(dog1.Health);
+            Console.ResetColor();
+        }
+       }
+    }
+}
